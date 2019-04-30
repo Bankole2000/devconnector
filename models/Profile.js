e351e2961dafc253de3765b01e1498221fdf4542
Esan Bankole <techybanky@gmail.com>
2019-04-30 03:45:49 +0100

    added user validation and profile model

---
+++ models/Profile.js
@@ -0,0 +1,120 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      }
+    company: {
+      type: String
+    },
+    location: {
+      type: String
+    },
+    from: {
+      type: String, 
+      required: true
+    },
+    to: {
+      type: Date
+    },
+    current: {
+      type: Boolean, 
+      default: false
+    },
+    description:{
+      type: String
+    }
+  }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      }
+    degree: {
+      type: String, 
+      required: true
+    },
+    fieldofstudy: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: String, 
+      required: true
+    },
+    to: {
+      type: Date
+    },
+    current: {
+      type: Boolean, 
+      default: false
+    },
+    description:{
+      type: String
+    }
+  }
+  ],
+  social: {
+    youtube: {
+      type: string
+    },
+    twitter: {
+      type: string
+    },
+    facebook: {
+      type: string
+    },
+    linkedin: {
+      type: string
+    },
+    instagram: {
+      type: string
+    },
+    date: {
+      type: Date, 
+      default: Date.now
+    }
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 // Load User model
 const User = require("../../models/User");
 
@@ -18,13 +22,17 @@ router.get("/test", (req, res) => res.json({ msg: "users works" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.send("Must include email and password");
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
   }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exits";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size 200 ox
@@ -56,6 +64,13 @@ router.post("/register", (req, res) => {
 // @desc    Login user / Returning JWT Token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -63,7 +78,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check Password
@@ -84,7 +100,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password Incorrect" });
+        errors.password = "Password Incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
