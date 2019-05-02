c538a51c5867136a4cfe2f425c31d43eaf77d9d3
Esan Bankole <techybanky@gmail.com>
2019-05-02 04:30:25 +0100

    Added post comments api

---
+++ validation/post.js
@@ -0,0 +1,21 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = "Post must be at least 10 and at most 300 character";
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
